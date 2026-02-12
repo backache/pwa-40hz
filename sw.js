be1ad287900433d6a1389166892dc3175dadaa63
Neil Carmichael <back_ache@hotmail.com>
2026-02-12 13:07:28 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "tone50hz-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    return cached || fetch(event.request);
+  })());
+});
